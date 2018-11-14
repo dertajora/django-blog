7e08ab35304d2e079cf7f52ba4aad29041d13fff
Derta Isyajora <derta.isyajora@gmail.com>
2018-11-14 14:34:54 +0700

    Add example of unit testing

---
+++ test/test_views.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestBasic(unittest.TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
+
+    def test_basic_2(self):
+        a = 1
+        assert 1 == 1
+
+    def test_basic_3(self):
+        a = 1
+        assert a == 1
